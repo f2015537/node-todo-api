e10db22fddfff437b00a1590250fbe5ed837a94b
f2015537 <divyampatro1997@gmail.com>
2018-11-09 15:19:54 +0530

    Explored mongoose Schemas,types and validators

--- server/server.js
+++ server/server.js
@@ -3,18 +3,30 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://localhost:27017/TodoApp',  { useNewUrlParser: true })
 
-const Todo = mongoose.model('Todo', {
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
-    completedAt: {
-        type: Number
+// const Todo = mongoose.model('Todo', {
+//     text: {
+//         type: String,
+//         required: true,
+//         minlength: 1,
+//         trim: true
+//     },
+//     completed: {
+//         type: Boolean,
+//         default: false
+//     },
+//     completedAt: {
+//         type: Number,
+//         default: null
+//     }
+// })
+const User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
     }
 })
-
 // const newTodo = new Todo({
 //     text: 'Cook Dinner'
 // })
@@ -23,12 +35,10 @@ const Todo = mongoose.model('Todo', {
 //     console.log('Saved todo', doc)
 // }).catch((err) => console.log('Unable to save todo'))
 
-const newTodo = new Todo({
-    text: 'Eat Healthy',
-    completed: true,
-    completedAt: 180397
+const user = new User({
+   
 })
 
-newTodo.save().then((doc) => {
+user.save().then((doc) => {
     console.log('Saved todo', doc)
-}).catch((err) => console.log('Unable to save todo'))
+}).catch((err) => console.log(err))
